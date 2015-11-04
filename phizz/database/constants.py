1c15c85c4e1af4d91b2b9f9a85c5f4d877cd4631
moonso <monsunas@gmail.com>
2015-11-04 13:40:25 +0100

    Fixed import errors and bumped to 0.0.3

--- phizz/database/constants.py
+++ phizz/database/constants.py
@@ -2,35 +2,26 @@ import os
 import pkg_resources
 import phizz
 
-resource_package = os.path.join(os.path.abspath(phizz.__name__), 'resources')
+resource_package = phizz.__name__
 
-disease_to_genes_path = os.path.join(
-    resource_package, 
-    'ALL_SOURCES_ALL_FREQUENCIES_diseases_to_genes_to_phenotypes.txt.gz'
+disease_to_genes_path = pkg_resources.resource_filename(
+    resource_package,
+    "resources/ALL_SOURCES_ALL_FREQUENCIES_diseases_to_genes_to_phenotypes.txt.gz"
 )
-phenotype_to_genes_path = os.path.join(
-    resource_package, 
-    'ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt.gz'
+phenotype_to_genes_path = pkg_resources.resource_filename(
+    resource_package,
+    "resources/ALL_SOURCES_ALL_FREQUENCIES_phenotype_to_genes.txt.gz"
 )
-schema_path = os.path.join(
-    resource_package, 
-    'schema.sql'
+schema_path = pkg_resources.resource_filename(
+    resource_package,
+    "resources/schema.sql"
 )
-phizz_db = os.path.join(
-    resource_package, 
-    'phizz.db'
+phizz_db = pkg_resources.resource_filename(
+    resource_package,
+    "resources/phizz.db"
 )
-# disease_to_genes_file = pkg_resources.resource_string(
-#     resource_package,
-#     disease_to_genes_path
-# )
-# phenotype_to_genes_file = pkg_resources.resource_string(
-#     resource_package,
-#     phenotype_to_genes_path
-# )
-# schema_file = pkg_resources.resource_string(
-#     resource_package,
-#     schema_path
-# )
 
-config_file = os.path.join(resource_package, "config.ini")
+config_file = pkg_resources.resource_filename(
+    resource_package, 
+    "resources/config.ini"
+)
